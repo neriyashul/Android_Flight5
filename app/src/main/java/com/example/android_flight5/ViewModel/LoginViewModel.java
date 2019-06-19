15d3fa6ce5d4e693b4f9276df0952b6cb7cae797
shulman pc <neriyashul@gmail.com>
2019-06-19 21:20:33 +0300

    add joystick

---
+++ app/src/main/java/com/example/android_flight5/ViewModel/LoginViewModel.java
@@ -0,0 +1,10 @@
+package com.example.android_flight5.ViewModel;
+
+import android.view.View;
+
+public class LoginViewModel {
+
+    public void onClick(View v) {
+        // Code here executes on main thread after user presses button
+    }
+}
