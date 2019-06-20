d6673435fcac8cad1e5a9ec17160186b86f5dfef
shilo gavra <shilo.gavra@live.biu.ac.il>
2019-06-20 16:44:43 +0300

    button bind

--- app/src/main/java/com/example/android_flight5/view_model/LoginViewModel.java
+++ app/src/main/java/com/example/android_flight5/view_model/LoginViewModel.java
@@ -1,9 +1,11 @@
-package com.example.android_flight5.ViewModel;
+package com.example.android_flight5.view_model;
 
+import android.util.Log;
 import android.view.View;
 
 public class LoginViewModel {
     public void onClick(View v) {
         // Code here executes on main thread after user presses button
+        Log.i("shilo", "button had clicked");
     }
 }
