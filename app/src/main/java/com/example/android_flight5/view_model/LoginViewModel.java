3d36475c63c11df51084070a8caa981c16256fdd
shilo gavra <shilo.gavra@live.biu.ac.il>
2019-06-20 17:15:20 +0300

    button bind

--- app/src/main/java/com/example/android_flight5/view_model/LoginViewModel.java
+++ app/src/main/java/com/example/android_flight5/view_model/LoginViewModel.java
@@ -1,11 +1,21 @@
 package com.example.android_flight5.view_model;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+
+import com.example.android_flight5.JoystickActivity;
 
 public class LoginViewModel {
     public void onClick(View v) {
         // Code here executes on main thread after user presses button
         Log.i("shilo", "button had clicked");
+
+        Context context = v.getContext();
+
+        Intent intent = new Intent(context, JoystickActivity.class);
+        //intent.putExtra("productId", productId);
+        context.startActivity(intent);
     }
 }
